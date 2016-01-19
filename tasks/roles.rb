569d07ba06b90d612db45c93b64c38e9714a3ddc
Pascal Morillon <pascal.morillon@irisa.fr>
2016-01-19 12:08:29 +0100

    [Rake] Add roles:show task

--- tasks/roles.rb
+++ tasks/roles.rb
@@ -4,3 +4,13 @@ role 'all' do
   roles 'puppetserver'
 end
 
+namespace :roles do
+
+  desc 'Show roles'
+  task :show do
+    XP5K::Role.list.each do |role|
+      puts "* Role #{role.name} : #{role.servers.join(' ,')}"
+    end
+  end
+
+end
